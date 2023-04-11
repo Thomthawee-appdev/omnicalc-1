b752c046091ab855496dbf2214b3cfab7ec3f24a
Thomthawee Leelahalamlert <thomthawee@uchicago.edu>
2023-04-11 17:27:40 +0000

    30.47 minute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   def blank_square_form
 
     render({ :template => "calculation_templates/square_form.html.erb"})
+  end
 
-  
+  def calculate_square
+    #params = {"elephant" => "42"}
+    @num = params.fetch("elephant").to_i
+    @square_of_num = @num * @num
 
+    render({ :template => "calculation_templates/square_results.html.erb"})
   end
 
 end
