54ff946dac55aea46ddf00cb94a948c056c47e94
Thomthawee Leelahalamlert <thomthawee@uchicago.edu>
2023-04-11 22:44:43 +0000

    75% done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,35 @@ class ApplicationController < ActionController::Base
 
   def calculate_square
     #params = {"elephant" => "42"}
-    @num = params.fetch("elephant").to_i
+    @num = params.fetch("elephant").to_f
     @square_of_num = @num * @num
 
     render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_random_form
+
+    render({ :template => "calculation_templates/rand_form.html.erb"})
+  
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/rand_results.html.erb"})
+  end
+
+  def blank_square_root_form
+  
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_square_root
+    @square_num = params.fetch("square_root_number").to_f
+    
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
   end
 
 end
